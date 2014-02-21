a08c0fffa13c4560ac4d1aba73e7993dfce940e2
Marie Hogebrandt <marie@pineberry.com>
2014-02-21 15:46:40 +0100

    Create Assemble Target
    Add JIT-task loader and set up the initial targets for Assemble. The
    build folder is "html", but as the different subprojects get merged,
    this repository will be pure HTML/JS/CSS

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: require('./package'),
+        bower: require('./bower'),
+        directories: {
+            flatBuild: {
+                base: 'src',
+                build: 'html'
+            }
+        },
+        files: {
+
+        }
+    });
+
+    // show elapsed time at the end
+    require('time-grunt')(grunt);
+    // load grunt tasks "just in time"
+    require('jit-grunt')(grunt);
+    grunt.loadTasks('grunt/tasks');
+}
---
+++ grunt/tasks/assemble.js
@@ -0,0 +1,25 @@
+module.exports = function (grunt) {
+    'use strict';
+
+    grunt.config('assemble', {
+        options: {
+            flatten: true,
+            layout: 'default.hbs',
+            layoutdir: '<%= directories.flatBuild.base %>/templates/layouts',
+            partials: ['<%= directories.flatBuild.base %>/templates/partials/*.hbs'],
+            helpers: '<%= directories.flatBuild.base %>/templates/helpers/*.js',
+            pkg: '<%= pkg %>',
+            data: '<%= directories.flatBuild.base %>/data/*.{json,yml}',
+        },
+        site: {
+            files: {
+                '<%= directories.flatBuild.build %>/': [
+                    '<%= directories.flatBuild.base %>/content/*.hbs'
+                ],
+                '<%= directories.flatBuild.build %>/mockup/': [
+                    '<%= directories.flatBuild.base %>/content/mockup/*.hbs'
+                ]
+            }
+        }
+    });
+};
